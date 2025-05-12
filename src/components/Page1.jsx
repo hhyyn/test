02d2b6476164f1e0e541548b84c9b29ac5bbeca1
hhyyn <aeronaer9@gmail.com>
2025-05-13 02:20:59 +0900

    페이지 이름 변경

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -3,8 +3,8 @@ import ZoomTable from './ZoomTable'
 const Page1 = () => {
   return (
     <div className="page">
-      <h1>페이지 1</h1>
-      <p>시안 1: 슬라이더로 테이블의 가로 스크롤을 제어할 수 있습니다.</p>
+      <h1>시안 1</h1>
+      <p>슬라이더로 테이블의 가로 스크롤을 제어</p>
       <ZoomTable designType={1} />
     </div>
   )
--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -3,8 +3,8 @@ import ZoomTable from './ZoomTable'
 const Page2 = () => {
   return (
     <div className="page">
-      <h1>페이지 2</h1>
-      <p>시안 2: edit/view 모드로 테이블의 가로 스크롤과 확대/축소 기능을 제어할 수 있습니다.</p>
+      <h1>시안 2</h1>
+      <p>edit/view 모드로 테이블의 가로 스크롤과 확대/축소 기능을 제어할 수 있음</p>
       <ZoomTable designType={2} />
     </div>
   )
